35b262b219a46d42952eb8b9d512289515c2df64
Sultanpopusk <agalalu04@gmail.com>
2023-10-14 11:52:45 +0300

    Update Gyro.java
    java1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Components/Sensors/Gyro.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Components/Sensors/Gyro.java
@@ -1,4 +1,25 @@
 package org.firstinspires.ftc.teamcode.Components.Sensors;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 
 public class Gyro {
+    public BNO055IMU imu;
+
+    public Gyro(HardwareMap hardwareMap){
+        imu = hardwareMap.get(BNO055IMU.class, "gyro");
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        //parameters.calibrationDataFile = "Calibration.json";
+        imu.initialize(parameters);
+    }
+
+    public double getHeading(){
+        double angle = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle;
+        return angle;
+    }
+
 }
