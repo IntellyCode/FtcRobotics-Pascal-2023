ceda08d59e3a6973d7ef5341304e28349f724e61
PhotoResistor <denis.zenios@gmail.com>
2023-10-14 11:55:57 +0300

    OpMode for Gyro 01

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Testing/TestingGyro.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode.OpModes.Testing;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.Components.Sensors.Gyro;
+
+@TeleOp
+public class TestingGyro extends OpMode {
+    Gyro gyro;
+    @Override
+    public void init() {
+        gyro = new Gyro(hardwareMap);
+    }
+
+    @Override
+    public void loop() {
+        double angle = gyro.getHeading();
+        telemetry.addData("Angle",angle);
+    }
+}
