4c371cb4177c81a7a9815e3911e812d3d394637f
Sultanpopusk <agalalu04@gmail.com>
2024-01-15 17:36:55 +0200

    ObjectDetection(недоделано)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Testing/TestingCamera.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Testing/TestingCamera.java
@@ -3,23 +3,43 @@ package org.firstinspires.ftc.teamcode.OpModes.Testing;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.HardwareMap;
 
-import org.firstinspires.ftc.teamcode.Components.Sensors.Camera;
-import org.firstinspires.ftc.teamcode.TelemetryHelper;
+import org.firstinspires.ftc.teamcode.Components.Sensors.ObjectDetection;
+
 
 @TeleOp(name = "Testing Camera", group = "Testing")
 public class TestingCamera extends OpMode {
-    Camera camera;
+    ObjectDetection object = new ObjectDetection();
     @Override
     public void init() {
-        TelemetryHelper.initTelemetry(telemetry);
-        camera = new Camera(hardwareMap);
-        camera.start();
+        object.initTfod(hardwareMap);
     }
 
     @Override
     public void loop() {
-        TelemetryHelper.getDashboardTelemetry().addData("Telemtry helper",true);
+                object.telemetryTfod();
+                // Push telemetry to the Driver Station.
+                telemetry.update();
+                // Save CPU resources; can resume streaming when needed.
+                if (gamepad1.dpad_down) {
+                    object.visionPortal.stopStreaming();
+                } else if (gamepad1.dpad_up) {
+                    object.visionPortal.resumeStreaming();
+        }
+        object.visionPortal.close();
+
     }
+//    Camera camera;
+//    @Override
+//    public void init() {
+//        TelemetryHelper.initTelemetry(telemetry);
+//        camera = new Camera(hardwareMap);
+//        camera.start();
+//    }
+//
+//    @Override
+//    public void loop() {
+//        TelemetryHelper.getDashboardTelemetry().addData("Telemtry helper",true);
+//    }
+
 }
